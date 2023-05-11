715078561b0e6bfadc937712f90e0791365f3eee
KennethOlusegun <kennetholusegun@gmail.com>
2023-05-11 12:20:42 -0300

    chore: creating typeorm instancing and todo module

---
+++ backend/src/app/todo/todo.module.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
+
+@Module({
+	controllers: [ TodoController ],
+	providers: [ TodoService ],
+	exports: [ TodoService ],
+})
+export class TodoModule {}
