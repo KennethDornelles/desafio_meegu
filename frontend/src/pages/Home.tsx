2b1a42925d896ff7c01c016e7df3edf9689916fc
KennethOlusegun <88892775+KennethOlusegun@users.noreply.github.com>
2023-05-18 15:03:15 -0300

    feat: Setting the state of the Home component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,17 +1,30 @@
+import { useState } from 'react';
 import { Column, Text, Row, Input, Button, List } from 'components';
 
 export const Home = () => {
+  const [taskName, setTaskName] = useState('');
+  const [tasks, setTasks] = useState<{ label: string }[]>([]);
+
+  const handleOKButton = () => {
+    if(!taskName) return;
+
+    setTasks((previous) => {
+      const copy = [...previous];
+      copy.push({ label: taskName });
+      return copy;
+    });
+    setTaskName('');
+  };
+
   return (
     <Column width="600px" margin="0 auto">
-      <Text fontWeight="bold" fontSize="bodyLarge" my="10px" paddingLeft="10px">Tasks</Text>
+      <Text fontWeight="bold" fontSize="bodyLarge" my="10px" paddingLeft="10px">
+        Tasks
+      </Text>
       <Row width="100%">
-        <Input placeholder="Enter a task name here" />
-        <Button>OK</Button>
-        <List items={[
-          { label: 'Task 1' },
-          { label: 'Task 2' },
-          { label: 'Task 3' }
-        ]}/>
+        <Input placeholder="Enter a task name here" value={taskName} onChange={(e) => setTaskName(e.target.value)} />
+        <Button onClick={handleOKButton}>OK</Button>
+        <List items={tasks} />
       </Row>
     </Column>
   );
